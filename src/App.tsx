8f9f48c1b7430864490b342b771c5eae159666b7
Lixiang Jiang <lixiangjiang@Lixiangs-MacBook-Pro-13.local>
2021-02-07 15:56:00 -0500

    Added CSS style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import QuestionSet from './Components/QuestionSet'
 import { QuestionSetModel } from './Models/QuestionSetModel'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-  let questionSet = new QuestionSetModel();
+
+  let defQuestionSet = new QuestionSetModel();
+  defQuestionSet.Title = 'The first QuestionSet';
+  defQuestionSet.Id = 1;
+  defQuestionSet.QuestionList = [
+    {
+      Id: 1,
+      Text: '1 + 2 = ',
+      Result: 3
+    },
+    {
+      Id: 2,
+      Text: "6 " + "\u00f7" + " 3 = ",
+      Result: 2
+    },
+    {
+      Id: 3,
+      Text: '2 + 2 ' + "\u00d7" + ' 5 = ',
+      Result: 12
+    },
+    {
+      Id: 4,
+      Text: '(2 + 2) ' + "\u00d7" + ' 5 = ',
+      Result: 20
+    },
+  ];
+
+  const [questionSet, setQuestionSet] = useState(defQuestionSet);
   return (
     <div className="App">
-      <QuestionSet {...questionSet}/>
+      <QuestionSet {...questionSet} />
     </div>
   );
 }
